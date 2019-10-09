59dd8c72a583004992cceb05685c0d43bc9b1bea
Aya Ashour <ayashour77@gmail.com>
2019-10-09 20:44:24 +0200

    Add project files.

---
+++ CmsCoreIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CmsCoreIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
